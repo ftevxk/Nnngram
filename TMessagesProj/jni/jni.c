da4b8bb49b7a7adef24a082328dd393ee63121ce
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-02-19 01:04:56 +0800

    refactor: make it nagram, thou nnngram now
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/jni/jni.c
+++ TMessagesProj/jni/jni.c
@@ -27,10 +27,6 @@ jint JNI_OnLoad(JavaVM *vm, void *reserved) {
 		return -1;
 	}
 
-    if (!checkSignature(verify_signature(vm))) {
-        return JNI_ERR;
-    }
-
     if (imageOnJNILoad(vm, env) != JNI_TRUE) {
         return -1;
     }
@@ -56,7 +52,7 @@ JNIEXPORT void Java_org_telegram_messenger_Utilities_aesIgeEncryption(JNIEnv *en
     jbyte *what = (*env)->GetDirectBufferAddress(env, buffer) + offset;
     unsigned char *keyBuff = (unsigned char *)(*env)->GetByteArrayElements(env, key, NULL);
     unsigned char *ivBuff = (unsigned char *)(*env)->GetByteArrayElements(env, iv, NULL);
-    
+
     AES_KEY akey;
     if (!encrypt) {
         AES_set_decrypt_key(keyBuff, 32 * 8, &akey);
