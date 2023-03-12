c43f63a32674c53c20e07aa33b4a4bc3807d0fa4
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-12 19:18:47 +0800

    feat: filter zalgo
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
@@ -4351,6 +4351,8 @@ public class MessageObject {
 
         if (messageText == null) {
             messageText = "";
+        } else {
+            messageText = StringUtils.zalgoFilter(messageText);
         }
     }
 
