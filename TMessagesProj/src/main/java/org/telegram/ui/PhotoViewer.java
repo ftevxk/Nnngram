52828aee5f60e929b8c395e2905da695d0cdd54f
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-03-28 23:53:54 +0800

    feat: forward to saved in photo viewer
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
+++ TMessagesProj/src/main/java/org/telegram/ui/PhotoViewer.java
@@ -2071,6 +2071,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
     private final static int gallery_menu_paint = 18;
     private final static int gallery_menu_qr = 84;
     private final static int gallery_menu_translate = 91;
+    private final static int gallery_menu_save_messages = 92;
+
     private final static int gallery_menu_reply = 21;
     private final static int gallery_menu_loop = 22;
 
@@ -5525,6 +5527,9 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                         videoPlayer.setLooping(playerLooping);
                     }
                     loopItem.setEnabledByColor(playerLooping, 0xFFFFFFFF, 0xFF73B4EC);
+                } else if (id == gallery_menu_save_messages) {
+                    var accountInstance = AccountInstance.getInstance(currentAccount);
+                    accountInstance.getSendMessagesHelper().sendMessage(new ArrayList<>(Collections.singletonList(currentMessageObject)),accountInstance.getUserConfig().getClientUserId(),false, false, true, 0);
                 }
             }
 
@@ -5647,6 +5652,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
         menuItem.addSubItem(gallery_menu_masks2, R.drawable.msg_sticker, getString("ShowStickers", R.string.ShowStickers)).setColors(0xfffafafa, 0xfffafafa);
         //menuItem.addSubItem(gallery_menu_edit_avatar, R.drawable.photo_paint, LocaleController.getString("EditPhoto", R.string.EditPhoto)).setColors(0xfffafafa, 0xfffafafa);
         menuItem.addSubItem(gallery_menu_set_as_main, R.drawable.msg_openprofile, LocaleController.getString("SetAsMain", R.string.SetAsMain)).setColors(0xfffafafa, 0xfffafafa);
+        menuItem.addSubItem(gallery_menu_save_messages, R.drawable.msg_saved, LocaleController.getString("SaveMessages", R.string.saveMessages)).setColors(0xfffafafa, 0xfffafafa);
         menuItem.addSubItem(gallery_menu_delete, R.drawable.msg_delete, LocaleController.getString("Delete", R.string.Delete)).setColors(0xfffafafa, 0xfffafafa);
         menuItem.addSubItem(gallery_menu_cancel_loading, R.drawable.msg_cancel, LocaleController.getString("StopDownload", R.string.StopDownload)).setColors(0xfffafafa, 0xfffafafa);
         translateItem = menuItem.addSubItem(gallery_menu_translate, R.drawable.msg_translate, LocaleController.getString("TranslateMessage", R.string.TranslateMessage));
@@ -13473,6 +13479,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 galleryButton.setVisibility(View.GONE);
                 galleryGap.setVisibility(View.GONE);
                 menuItem.hideSubItem(gallery_menu_share);
+                menuItem.hideSubItem(gallery_menu_save_messages);
                 setItemVisible(editItem, false, animated);
                 if (!newMessageObject.canDeleteMessage(parentChatActivity != null && parentChatActivity.isInScheduleMode(), null)) {
                     menuItem.hideSubItem(gallery_menu_delete);
@@ -13580,6 +13587,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
             if (DialogObject.isEncryptedDialog(currentDialogId) && !isEmbedVideo || noforwards) {
                 setItemVisible(sendItem, false, false);
             }
+                menuItem.showSubItem(gallery_menu_save);
+                menuItem.showSubItem(gallery_menu_save_messages);
             if (isEmbedVideo || newMessageObject.messageOwner.ttl != 0 && newMessageObject.messageOwner.ttl < 60 * 60 || noforwards) {
                 allowShare = false;
                 galleryButton.setVisibility(View.GONE);
@@ -13601,6 +13610,7 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
             galleryButton.setVisibility(View.GONE);
             galleryGap.setVisibility(View.GONE);
             menuItem.hideSubItem(gallery_menu_translate);
+            menuItem.showSubItem(gallery_menu_save_messages);
             if (countView != null) {
                 countView.updateShow(secureDocuments.size() > 1, true);
                 countView.set(switchingToIndex + 1, secureDocuments.size());
@@ -13670,7 +13680,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
 
             galleryButton.setVisibility(View.VISIBLE);
             galleryGap.setVisibility(View.VISIBLE);
-            
+
+            menuItem.showSubItem(gallery_menu_save_messages);
             allowShare = !noforwards;
             menuItem.showSubItem(gallery_menu_share);
             menuItem.checkHideMenuItem();
@@ -13929,6 +13940,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
             if (currentAnimation != null || (!pageBlocksAdapter.isVideo(index) && pageBlocksAdapter.isHardwarePlayer(index))) {
                 galleryButton.setVisibility(View.GONE);
                 galleryGap.setVisibility(View.GONE);
+                menuItem.hideSubItem(gallery_menu_save);
+                menuItem.hideSubItem(gallery_menu_save_messages);
                 if (allowShare) {
                     menuItem.showSubItem(gallery_menu_savegif);
                 } else {
@@ -13946,6 +13959,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 }
                 galleryButton.setVisibility(View.VISIBLE);
                 galleryGap.setVisibility(View.VISIBLE);
+                menuItem.showSubItem(gallery_menu_save);
+                menuItem.showSubItem(gallery_menu_save_messages);
                 menuItem.hideSubItem(gallery_menu_savegif);
                 menuItem.checkHideMenuItem();
             }
@@ -14311,6 +14326,8 @@ public class PhotoViewer implements NotificationCenter.NotificationCenterDelegat
                 AndroidUtilities.cancelRunOnUIThread(hideActionBarRunnable);
             }
             if (sharedMediaType == MediaDataController.MEDIA_FILE) {
+                menuItem.showSubItem(gallery_menu_save);
+                menuItem.showSubItem(gallery_menu_save_messages);
                 if (canZoom = newMessageObject.canPreviewDocument()) {
                     galleryButton.setVisibility(View.VISIBLE);
                     galleryGap.setVisibility(View.VISIBLE);
