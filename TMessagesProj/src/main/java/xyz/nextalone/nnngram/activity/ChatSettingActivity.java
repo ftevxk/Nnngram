cec8a2aced7854193156be6a512d79d9ea7a38e4
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-01-01 02:10:33 +0800

    feat: search in place
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/xyz/nextalone/nnngram/activity/ChatSettingActivity.java
+++ TMessagesProj/src/main/java/xyz/nextalone/nnngram/activity/ChatSettingActivity.java
@@ -132,6 +132,7 @@ public class ChatSettingActivity extends BaseActivity {
     private int mergeMessageRow;
     private int filterZalgoRow;
     private int hideKeyboardWhenScrollingRow;
+    private int searchInPlaceRow;
     private int chat2Row;
 
     private int markdownRow;
@@ -433,6 +434,11 @@ public class ChatSettingActivity extends BaseActivity {
             if (view instanceof TextCheckCell) {
                 ((TextCheckCell) view).setChecked(Config.hideKeyboardWhenScrolling);
             }
+        } else if (position == searchInPlaceRow) {
+            Config.toggleSearchInPlace();
+            if (view instanceof TextCheckCell) {
+                ((TextCheckCell) view).setChecked(Config.searchInPlace);
+            }
         }
     }
 
@@ -495,6 +501,7 @@ public class ChatSettingActivity extends BaseActivity {
         mergeMessageRow = addRow("mergeMessage");
         filterZalgoRow = addRow("filterZalgo");
         hideKeyboardWhenScrollingRow = addRow("hideKeyboardWhenScrolling");
+        searchInPlaceRow = addRow("searchInPlace");
         chat2Row = addRow();
         markdownRow = addRow();
         markdownDisableRow = addRow("markdownDisabled");
@@ -659,6 +666,8 @@ public class ChatSettingActivity extends BaseActivity {
                         textCell.setTextAndCheck(LocaleController.getString("filterZalgo", R.string.filterZalgo), Config.filterZalgo, true);
                     } else if (position == hideKeyboardWhenScrollingRow) {
                         textCell.setTextAndCheck(LocaleController.getString("hideKeyboardWhenScrolling", R.string.hideKeyboardWhenScrolling), Config.hideKeyboardWhenScrolling, true);
+                    } else if (position == searchInPlaceRow) {
+                        textCell.setTextAndCheck(LocaleController.getString("searchInPlace", R.string.searchInPlace), Config.searchInPlace, true);
                     }
                     break;
                 }
