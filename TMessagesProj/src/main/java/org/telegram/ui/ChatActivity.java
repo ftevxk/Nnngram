7dfac3fcc38597cf14baacb78564643146a2369d
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-01-25 13:06:08 +0800

    fix: search error in some occasions

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -20655,10 +20655,6 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                     getMediaDataController().setCurrentMessage(nextLargerIndex);
                                     num = nextLargerIndex;
                                 }
-                            } else {
-                                messageId = lastFoundMessageId;
-                                num = foundMessageIds.size();
-                                mask = 2;
                             }
                         }
                     } else {
