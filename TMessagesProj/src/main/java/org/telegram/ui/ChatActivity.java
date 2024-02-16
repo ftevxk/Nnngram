191dc7f09110214e2da83b255b8ca26156872318
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-02-17 03:42:37 +0800

    feat: save webm and tgs to gif
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -27118,12 +27118,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 options.add(OPTION_ADD_TO_STICKERS_OR_MASKS);
                                 icons.add(R.drawable.msg_sticker);
                             } else {
-                                if (!selectedObject.isAnimatedSticker()) {
+//                                if (!selectedObject.isAnimatedSticker()) {
                                     items.add(LocaleController.getString("SaveToGallery",
                                         R.string.SaveToGallery));
                                     options.add(OPTION_SAVE_STICKER_TO_GALLERY);
                                     icons.add(R.drawable.msg_gallery);
-                                }
+//                                }
                                 items.add(LocaleController.getString("AddToStickers",
                                     R.string.AddToStickers));
                                 options.add(OPTION_ADD_TO_STICKERS_OR_MASKS);
@@ -27163,12 +27163,12 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 icons.add(R.drawable.msg_callback);
                             }
                         } else if (type == 9) {
-                            if (!selectedObject.isAnimatedSticker()) {
+//                            if (!selectedObject.isAnimatedSticker()) {
                                 items.add(LocaleController.getString("SaveToGallery",
                                     R.string.SaveToGallery));
                                 options.add(OPTION_SAVE_STICKER_TO_GALLERY);
                                 icons.add(R.drawable.msg_gallery);
-                            }
+//                            }
                             TLRPC.Document document = selectedObject.getDocument();
                             if (!getMediaDataController().isStickerInFavorites(document)) {
                                 if (MessageObject.isStickerHasSet(document)) {
