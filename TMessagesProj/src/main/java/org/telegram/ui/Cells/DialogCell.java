11baea2f97fecc7239a7667b0a20e76d0c7ad901
ftevxk <ftevxk@gmail.com>
2024-04-02 09:37:38 +0800

    列表图片/视频类型标题位置调整

--- TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
+++ TMessagesProj/src/main/java/org/telegram/ui/Cells/DialogCell.java
@@ -2106,12 +2106,11 @@ public class DialogCell extends BaseCell implements StoriesListPlaceProvider.Ava
             tagsRight = getMeasuredWidth() - dp(messagePaddingStart);
             tagsLeft = dp(64);
         } else {
-//          tagsLeft = messageLeft;
-//          tagsRight = getMeasuredWidth() - dp(64);
+            tagsLeft = messageLeft;
+            tagsRight = getMeasuredWidth() - dp(64);
             //wd 标题向右偏移位置
-            int thumbsWidth = thumbsCount <= 0 ? 0 :AndroidUtilities.dp(thumbSize + 2) * thumbsCount;
-            tagsLeft = messageLeft + thumbsWidth;
-            tagsRight = getMeasuredWidth() - dp(64) - thumbsWidth;
+            int thumbsWidth = thumbsCount <= 0 ? 0 : AndroidUtilities.dp(thumbSize + 2) * thumbsCount;
+            messageNameLeft += thumbsWidth;
         }
         if (twoLinesForName) {
             messageNameTop += dp(20);
