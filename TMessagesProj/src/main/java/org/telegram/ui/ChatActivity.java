572bed508e18fcb40372a734fb297e9dce5236be
Next Alone <12210746+NextAlone@users.noreply.github.com>
2024-04-12 01:21:27 +0800

    fix: in-place search stuck at 41 at most
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -8776,8 +8776,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                     updateSearchUpDownButtonVisibility(true);
                     updatePagedownButtonVisibility(true);
                     searchingQuery = searchItem.getSearchField().getText().toString();
+                    lastSearchedMessageId = 0;
                     getMediaDataController().searchMessagesInChat(searchingQuery, dialog_id, mergeDialogId, classGuid, 0, threadMessageId, false, searchingUserMessages, searchingChatMessages,
-                        !TextUtils.isEmpty(searchingQuery) || searchingReaction != null, searchingReaction, searchingType);
+                        !TextUtils.isEmpty(searchingQuery) || searchingReaction != null, searchingReaction, searchingType, true);
                     AndroidUtilities.hideKeyboard(searchItem.getSearchField());
                     return true;
                 }
@@ -10106,7 +10107,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                         updateSearchUpDownButtonVisibility(true);
                         updatePagedownButtonVisibility(true);
                         searchingQuery = searchItem.getSearchField().getText().toString();
-                        getMediaDataController().searchMessagesInChat(searchingQuery, dialog_id, mergeDialogId, classGuid, 0, threadMessageId, searchingUserMessages, searchingChatMessages, searchingReaction, searchingType);
+                        lastSearchedMessageId = 0;
+                        getMediaDataController().searchMessagesInChat(searchingQuery, dialog_id, mergeDialogId, classGuid, 0, threadMessageId, searchingUserMessages, searchingChatMessages,
+                         searchingReaction, searchingType, true);
                     });
                     if (searchingType == filter.second) {
                         filterPopup.putCheck();
@@ -22195,10 +22198,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 foundMessageIds.add(message.getId());
                             }
                             Collections.sort(foundMessageIds);
-                            int firstFoundMessageId = foundMessageIds.get(0);
-                            int lastFoundMessageId = foundMessageIds.get(foundMessageIds.size() -1);
-                            if (firstFoundMessageId > currentMessageId && lastSearchedMessageId != lastFoundMessageId) {
-                                lastSearchedMessageId = lastFoundMessageId;
+                            int minFoundMessageId = Collections.min(foundMessageIds);
+                            if (minFoundMessageId > currentMessageId && lastSearchedMessageId != minFoundMessageId) {
+                                lastSearchedMessageId = minFoundMessageId;
                                 Runnable loop = () -> {
                                     getMediaDataController().setCurrentMaxMessage();
                                     getMediaDataController().searchMessagesInChat(null, dialog_id, mergeDialogId, classGuid, 1, threadMessageId, searchingUserMessages,
@@ -22211,8 +22213,8 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 }
                                 return;
                             }
-
-                            if (currentMessageId < lastFoundMessageId) {
+                            int maxFoundMessageId = Collections.max(foundMessageIds);
+                            if (currentMessageId < maxFoundMessageId) {
                                 int insertionPoint = Collections.binarySearch(foundMessageIds, currentMessageId);
                                 int nextLargerIndex = (insertionPoint < 0) ? Math.max(-(insertionPoint + 1) - 1, 0) : insertionPoint;
                                 if (nextLargerIndex < foundMessageIds.size()) {
@@ -22227,7 +22229,6 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
                                 }
                             }
                         }
-                    } else {
                     }
                     long did = (Long) args[3];
 
