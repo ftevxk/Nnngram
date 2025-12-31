47892dd38a759556e2de7248432f4dcdba413d5c
ftevxk <2116899562@qq.com>
2025-12-31 17:53:42 +0800

    refactor(DialogsActivity): 移除浮动按钮的实验性功能代码
    移除浮动按钮点击事件中关于搜索视频最小时长弹框的实验性功能代码，保持功能简洁

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -4679,8 +4679,6 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
         floatingButtonContainer.setVisibility(onlySelect && initialDialogsType != 10 || folderId != 0 ? View.GONE : View.VISIBLE);
         contentView.addView(floatingButtonContainer, LayoutHelper.createFrame(56, 56, (LocaleController.isRTL ? Gravity.LEFT : Gravity.RIGHT) | Gravity.BOTTOM, LocaleController.isRTL ? 14 : 0, 0, LocaleController.isRTL ? 0 : 14, 14));
         floatingButtonContainer.setOnClickListener(v -> {
-            //wd 修改浮动按钮跳转为显示设置搜索视频最小时长弹框
-            ExperimentSettingActivity.showSearchVideoMinDurationDialog(this, null, null, 0);
 //            if (parentLayout != null && parentLayout.isInPreviewMode()) {
 //                finishPreviewFragment();
 //                return;
